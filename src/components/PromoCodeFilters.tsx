4a90267d5a37dfdbfc49f052d20618d2d12a73c9
Mobkus <tagankhozhaev@gmail.com>
2025-05-28 10:29:28 +0500

    feat: added promo-codes filtration

--- src/components/PromoCodeFilters.tsx
+++ src/components/PromoCodeFilters.tsx
@@ -1,7 +1,22 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import {AddAdminModal} from "@/components/AddAdminModal.tsx";
+import {usePromoCodesStore} from "@/store/usePromoCodesStore.ts";
+import {CustomButton} from "@/ui/CustomButton.tsx";
 
 export const PromoCodeFilters:FC = () => {
+    const [promo, setPromo] = useState("");
+
+    const {fetchPromoCodes} = usePromoCodesStore();
+
+    const handleSearch = async () => {
+        await fetchPromoCodes({
+            code: promo || undefined,
+            page: 0,
+            size: 10,
+        })
+    }
+
+
     return (
         <div className="flex justify-between items-end mb-[31px]">
             <div className="flex gap-[22px]">
@@ -14,21 +29,20 @@ export const PromoCodeFilters:FC = () => {
                     />
                 </div>
                 <div className="flex flex-col gap-[10px]">
-                    <label>ID</label>
-                    <input
-                        type="text"
-                        className="bg-[#FFFFFF] h-[37px] p-2 border-1 rounded-[4px] border-[#6B9AB0]"
-                        placeholder="Enter Invoice ID"
-                    />
-                </div>
-                <div className="flex flex-col gap-[10px]">
                     <label>Promo code</label>
                     <input
                         type="text"
+                        onChange={(e) => setPromo(e.target.value)}
                         className="bg-[#FFFFFF] h-[37px] p-2 border-1 rounded-[4px] border-[#6B9AB0]"
                         placeholder="Enter Promo code"
                     />
                 </div>
+                <CustomButton
+                    onClick={handleSearch}
+                    className="h-[37px] px-4 mt-auto text-white rounded-[4px]  transition"
+                >
+                    Search
+                </CustomButton>
             </div>
             <AddAdminModal />
         </div>
