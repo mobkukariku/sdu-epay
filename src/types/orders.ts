473258610428ca35aae582ab125af84fe91e63ec
Mobkus <tagankhozhaev@gmail.com>
2025-06-02 20:23:18 +0500

    feat: added kaspi post api

--- src/types/orders.ts
+++ src/types/orders.ts
@@ -1,8 +1,39 @@
 export interface IOrder {
     event_id: string;
-    promo_code: string;
+    promo_code?: string; // сделаем опциональным
     fullname: string;
     email: string;
     cellphone: string;
     additional: string;
+}
+
+
+
+export interface PaymentResponseKaspi {
+    id: string;
+    amount_tiyn: number;
+    code: number;
+    redirect_url: string;
+    message: string;
+    qr_code_image: string;
+    device: "PC" | "MOBILE" | string; // если возможны другие типы — оставляем string
+    order: {
+        id: number;
+        fullname: string;
+        email: string;
+        cellphone: string;
+        additional: string;
+        type: "KASPI" | "CARD" | string; // добавьте другие варианты, если есть
+        status: "PENDING" | "PAID" | "FAILED" | string; // добавьте другие варианты, если есть
+        amount: number;
+        final_amount: number;
+        event: {
+            title: string;
+            manager_email: string;
+            price: number;
+            period_from: string; // можно заменить на Date, если преобразуете при получении
+            period_till: string;
+            id: string;
+        };
+    };
 }
